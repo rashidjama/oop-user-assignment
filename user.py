5a7fa44053dce32ebc65ce670f03bbb9113958fe
Rashid Jama <rashidjama5@gmail.com>
2020-06-09 09:16:06 -0700

    chaing methods

--- user.py
+++ user.py
@@ -8,14 +8,18 @@ class User:
     #methods
   def make_deposit(self, amount):
     self.balance += amount
+    return self
     
     # Add a make_withdrawal method to the User class
   def make_withdrawal(self, amount):
     self.balance -= amount
+    return self
 
     # Add a display_user_balance method to the User class  
   def display_user_balance(self):
-    print(f"Balance: {self.balance}")
+    print(f"{self.name} has a balance of: {self.balance}")
+    
+    #transfer money from one account to another Method
   def transfer_money(self, amount,reciever):
     self.make_withdrawal(amount)
     reciever.make_deposit(amount)
@@ -26,31 +30,16 @@ jane = User("Jane", "jane@hotmail.com")
 doe = User("Doe", "doe@yahoo.com")
 
 # Have the first user make 3 deposits and 1 withdrawal and then display their balance
-john.make_deposit(500)
-john.make_deposit(700)
-john.make_deposit(2000)
 
-# john.make_withdrawal(80)
-
-# john.display_user_balance()
+john.make_deposit(500).make_deposit(700).make_deposit(2000).make_withdrawal(100).display_user_balance()
 
 # Have the second user make 2 deposits and 2 withdrawals and then display their balance
-jane.make_deposit(1200)
-jane.make_deposit(300)
 
-jane.make_withdrawal(400)
-jane.make_withdrawal(100)
-
-# jane.display_user_balance()
+jane.make_deposit(1200).make_deposit(300).make_withdrawal(400).make_withdrawal(100).display_user_balance()
 
 # Have the third user make 1 deposits and 3 withdrawals and then display their balance 
-doe.make_deposit(5000)
 
-doe.make_withdrawal(4000)
-doe.make_withdrawal(300)
-doe.make_withdrawal(300)
-
-# doe.display_user_balance()
+doe.make_deposit(5000).make_withdrawal(4000).make_withdrawal(300).make_withdrawal(300).display_user_balance()
 
 
 # BONUS: Add a transfer_money method; have the first user transfer money to the third user and then print both users' balances
@@ -59,8 +48,8 @@ doe.make_withdrawal(300)
 #Transfer money
 john.transfer_money(1200, doe)
 
-# john.display_user_balance()
+john.display_user_balance()
 
-# doe.display_user_balance()
+doe.display_user_balance()
 
 
