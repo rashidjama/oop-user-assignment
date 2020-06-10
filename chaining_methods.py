f8ba9d3968272d2a246ecc2ac167c2f382d8ef59
Rashid Jama <rashidjama5@gmail.com>
2020-06-09 18:46:25 -0700

    final commit of user

--- chaining_methods.py
+++ chaining_methods.py
@@ -8,12 +8,12 @@ class User:
     #methods
   def make_deposit(self, amount):
     self.balance += amount
-    
+    return self
     
     # Add a make_withdrawal method to the User class
   def make_withdrawal(self, amount):
     self.balance -= amount
- 
+    return self
 
     # Add a display_user_balance method to the User class  
   def display_user_balance(self):
@@ -31,35 +31,25 @@ doe = User("Doe", "doe@yahoo.com")
 
 # Have the first user make 3 deposits and 1 withdrawal and then display their balance
 
-john.make_deposit(500)
-john.make_deposit(700)
-john.make_deposit(2000)
-john.make_withdrawal(100)
-john.display_user_balance()
+john.make_deposit(500).make_deposit(700).make_deposit(2000).make_withdrawal(100).display_user_balance()
 
 # Have the second user make 2 deposits and 2 withdrawals and then display their balance
 
-jane.make_deposit(1200)
-jane.make_deposit(300)
-jane.make_withdrawal(400)
-jane.make_withdrawal(100)
-jane.display_user_balance()
+jane.make_deposit(1200).make_deposit(300).make_withdrawal(400).make_withdrawal(100).display_user_balance()
 
 # Have the third user make 1 deposits and 3 withdrawals and then display their balance 
 
-doe.make_deposit(5000)
-doe.make_withdrawal(4000)
-doe.make_withdrawal(300)
-doe.make_withdrawal(300)
-doe.display_user_balance()
+doe.make_deposit(5000).make_withdrawal(4000).make_withdrawal(300).make_withdrawal(300).display_user_balance()
 
 
 # BONUS: Add a transfer_money method; have the first user transfer money to the third user and then print both users' balances
 
-#BONUS
+
 #Transfer money
 john.transfer_money(1200, doe)
 
 john.display_user_balance()
 
 doe.display_user_balance()
+
+
